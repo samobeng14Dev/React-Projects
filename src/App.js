837880a94984b3b8575abd47745a74bbbef100f9
samuelobeng102@gmail.com <obengsamuel270@gmail.com>
2023-10-02 02:07:34 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+function App() {
+	return (
+		<>
+			<Navbar />
+			<Hero />
+			<About />
+			<Services />
+			<Tours />
+			<Footer />
+		</>
+	);
+}
+export default App;
